3d7392de305b5fc5de2d7fb96459aca0cc124952
René Navarro <rene.navarro@gmail.com>
2024-08-10 10:43:21 -0700

    Poyecto base del proyecto de códigos postales.

---
+++ src/main/java/mx/unison/Main.java
@@ -0,0 +1,12 @@
+package mx.unison;
+
+/* Contar cuantos códigos postales corresponden a asentamientos rurales
+y cantos a asentamientos rurales
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
